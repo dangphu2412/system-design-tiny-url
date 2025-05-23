ab87379bd127c39918acc20efb15683ead5376fc
dangphu2412 <dangphu241299@gmail.com>
2025-05-23 21:24:54 +0700

    feat: replace html file

--- performances/locustfile.py
+++ performances/locustfile.py
@@ -1,8 +1,26 @@
 from locust import HttpUser, task, between
+import random
+import string
+
+def random_url():
+    return "http://localhost" + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
 class UrlPostUser(HttpUser):
-    wait_time = between(1, 2)  # seconds between tasks
+    wait_time = between(1, 2)# seconds between tasks
+
+    def on_start(self):
+        self.short_codes = ['0000001']
 
     @task
     def post_url(self):
-        self.client.post("/urls", json={"url": "https://docs.locust.io/en/stable/running-in-docker.html"})
+        response = self.client.post("/urls", json={"url": random_url()})
+        if response.status_code == 200:
+            short_code = response.json().get("id")
+            if short_code:
+                self.short_codes.append(short_code)
+
+    @task(2)  # 2x more read requests
+    def read_url(self):
+        short_code = random.choice(self.short_codes)
+        self.client.get(f"/urls/{short_code}", name="/:shortCode")
+
