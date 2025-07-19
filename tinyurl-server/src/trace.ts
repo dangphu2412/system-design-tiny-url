096d4888f264b2696254e3693acdbfb76c137758
dangphu2412 <dangphu241299@gmail.com>
2025-07-19 15:38:15 +0700

    feat: add service name

--- tinyurl-server/src/trace.ts
+++ tinyurl-server/src/trace.ts
@@ -4,17 +4,15 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import 'dotenv/config';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 const { credentials } = require('@grpc/grpc-js');
-// [START opentelemetry_instrumentation_setup_opentelemetry]
 
 diag.setLogger(new DiagConsoleLogger());
 
 const sdk = new opentelemetry.NodeSDK({
+  serviceName: 'tinyurl-service',
   instrumentations: getNodeAutoInstrumentations({
     // Disable noisy instrumentations
     '@opentelemetry/instrumentation-fs': { enabled: false },
   }),
-  // resourceDetectors: getResourceDetectorsFromEnv(),
-  // metricReader: getMetricReader(),
   traceExporter: new OTLPTraceExporter({
     url: process.env.OLTP_EXPORTER,
     credentials: credentials.createInsecure(),
@@ -38,5 +36,3 @@ process.on('SIGTERM', () => {
     .then(() => diag.debug('OpenTelemetry SDK terminated'))
     .catch((error) => diag.error('Error terminating OpenTelemetry SDK', error));
 });
-
-// [END opentelemetry_instrumentation_setup_opentelemetry]
